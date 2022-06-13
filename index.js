1b10ca5c8ff98126d6260be516f0db82d18d2c64
Jaime Irazabal <jaimeirazabal1@sistemaimpulsa.com>
2022-06-12 23:01:31 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send(req.body)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
